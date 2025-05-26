030102633c1651097f1247f111ebfed6f60e04d8
sudhanv09 <mystogun125@gmail.com>
2025-05-27 00:09:07 +0800

    interacting with mpv over IPC

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 mod indexer;
+mod mpv;
 
-fn main() {
+#[tokio::main]
+async fn main() -> tokio::io::Result<()> {
     let index = indexer::index(
         String::from("/hdd/media/Movies"),
         String::from("/hdd/media/TV"),
     );
+
+    let mut mpv = mpv::Player::init("/tmp/mpvipc").await?;
+
+    println!("{:?}", &index.movies[0].name);
+    mpv.play_file(&index.movies[0].path).await?;
+
+    Ok(())
 }
--- src/mpv.rs
+++ src/mpv.rs
@@ -1,3 +1,19 @@
+use serde::{Deserialize, Serialize};
+use std::path::Path;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::UnixStream;
+
+#[derive(Debug, Serialize)]
+struct MpvCommand {
+    command: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct MpvResponse {
+    data: Option<serde_json::Value>,
+    request_id: Option<i32>,
+    error: Option<String>,
+}
 
 pub struct Player {
     socket: UnixStream,
@@ -21,17 +37,31 @@ impl Player {
         Ok(serde_json::from_str(&response)?)
     }
 
-    async fn get(&mut self, prop: &str) -> tokio::io::Result<MpvResponse> {
-        self.send_command(vec!["get_property".into(), prop.into()]).await
+    async fn get_property(&mut self, prop: &str) -> tokio::io::Result<MpvResponse> {
+        self.send_command(vec!["get_property".into(), prop.into()])
+            .await
     }
 
-    async fn set(&mut self, prop: &str, value: &str) -> tokio::io::Result<MpvResponse> {
-        self.send_command(vec!["set_property".into(), prop.into(), value.into()]).await
+    async fn set_property(&mut self, prop: &str, value: &str) -> tokio::io::Result<MpvResponse> {
+        self.send_command(vec!["set_property".into(), prop.into(), value.into()])
+            .await
     }
 
-    pub async fn play_file(&mut self, path: &str) -> tokio::io::Result<()> {
-        self.send_command(vec!["loadfile".into(), path.into(), "replace".into()])
+    pub async fn play_file<P: AsRef<Path>>(&mut self, path: P) -> tokio::io::Result<()> {
+        let path_str = path.as_ref().to_string_lossy();
+        self.send_command(vec!["loadfile".into(), path_str.into_owned(), "replace".into()])
             .await?;
         Ok(())
     }
+
+    pub async fn get_position(&mut self) -> tokio::io::Result<Option<f64>> {
+        let response = self.get_property("time-pos").await?;
+        if let Some(data) = response.data {
+            if let Some(pos) = data.as_f64() {
+                return Ok(Some(pos));
+            }
+        }
+        Ok(None)
+    }
+
 }
