7f9f8968c1a45ee2419d8ff42eddaa6ad6f1fc8d
sudhanv09 <mystogun125@gmail.com>
2025-05-27 22:16:45 +0800

    adding clap and commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 mod indexer;
-mod mpv;
 mod library;
+mod mpv;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "pmc")]
+#[command(about = "simple CLI tool to manage your media", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    List {
+        #[command(subcommand)]
+        list_command: ListSubcommand,
+    },
+}
+
+#[derive(Subcommand)]
+pub enum ListSubcommand {
+    Recent,
+    Library,
+}
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
